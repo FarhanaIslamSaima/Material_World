7fa3bbebcf2774bec0b633ff288b16cdf3ac8cd5
abar.toha <abar.toha@gmail.com>
2023-07-25 04:55:45 +0600

    test html view

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,7 +1,7 @@
 export const textCheck=async(request,response)=>{
     try{
-        console.log("Hello whatsapp");
-        response.status(200).json("Hello whatsapp");
+        console.log("Hello Whatsapp");
+        response.status(200).json("Hello Whats Up");
 
     }
     catch(error){
