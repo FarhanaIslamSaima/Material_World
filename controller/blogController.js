c549b83f6765ebdde352fa5a00f87604a41bba38
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2024-02-03 07:55:33 +0600

    updated route

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,3 +1,4 @@
+
 import blogmodel from "../model/blogModel.js";
 
 export const addPost=async(req,res)=>{
@@ -34,7 +35,72 @@ export const getDatabyCat=async(request,response)=>{
 
     }
     catch(error){
-        response.json(500).json(error);
+        response.status(500).json(error);
+    }
+
+}
+export const getPostMostRecent=async(request,response)=>{
+    try{
+        const data=await blogmodel.find({cat:request.params.cat}).sort({createdAt:'desc'})
+       response.status(200).json(data);
+
+    }
+    catch(error){
+        response.status(500).json(error);
+
+    }
+
+}
+export const getBreaknews=async(request,response)=>{
+    try{
+        const data=await blogmodel.find({tag:"Breaking News"})
+        response.status(200).json(data)
+
+    }
+    catch(error){
+        response.status(500).json(error)
+    }
+
+}
+export const getAllMostRecent=async(request,response)=>{
+    try{
+        const data=await blogmodel.find({}).sort({createdAt:'desc'}).limit(6)
+        response.status(200).json(data);
+        
+
+    }
+    catch(error){
+        response.status(500).json(error)
+    }
+
+}
+export const getMostRecentOne=async()=>{
+    try{
+        const data=await blogmodel.find({}).sort({createdAt:'desc'}).limit(1);
+
+    }
+    catch(error){
+        response.status(500).json(error)
+    }
+
+}
+export const getFeatureMostRecentOne=async()=>{
+    try{
+        const data=await blogmodel.find({tag:'Feature'}).sort({createdAt:'desc'}).limit(1);
+
+    }
+    catch(error){
+        response.status(500).json(error)
+    }
+
+}
+export const getInternationaleMostRecentOne=async()=>{
+    try{
+        const data=await blogmodel.find({tag:'International'}).sort({createdAt:'desc'}).limit(1);
+
+    }
+    catch(error){
+        response.status(500).json(error)
     }
 
 }
--- route/Router.js
+++ route/Router.js
@@ -1,7 +1,14 @@
 import express from 'express'
-import { addPost,getPost,getDatabyCat } from '../controller/blogController.js';
+import { addPost,getPost,getDatabyCat,getPostMostRecent ,getBreaknews, getAllMostRecent,getMostRecentOne,getFeatureMostRecentOne,getInternationaleMostRecentOne} from '../controller/blogController.js';
+
 const Route=express.Router();
 Route.get("/get/news/:cat",getDatabyCat);
+Route.get("/get/news/recent/:cat",getPostMostRecent);
+Route.get("/get/breaknews",getBreaknews)
+Route.get("/all/mostrecent",getAllMostRecent)
+Route.get("/one/recent",getMostRecentOne)
+Route.get("/one/feature",getFeatureMostRecentOne)
+Route.get("/one/international",getInternationaleMostRecentOne)
 
 
 export default Route;
